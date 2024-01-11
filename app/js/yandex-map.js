b625a683cafb544d6ed5fae4bca7f3f6ecf43028
Kie6er <kirill.zawgorodny@yandex.ru>
2024-01-11 16:48:04 +0300

    feat: modal-map

--- app/js/yandex-map.js
+++ app/js/yandex-map.js
@@ -1,9 +1,11 @@
-if (document.querySelector('#map')) {
-  ymaps.ready(function () {
-    let map = new ymaps.Map('map', {
-      center: [55.754203, 37.559988],
-      zoom: 16,
-      controls: [],
-    });
-  });
-}
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('#map')) {
+        ymaps.ready(function () {
+            let map = new ymaps.Map('map', {
+                center: [55.754203, 37.559988],
+                zoom: 16,
+                controls: []
+            });
+        });
+    }
+});
